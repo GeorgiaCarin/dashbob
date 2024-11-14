ee500fc45ce73fe00af7f82166a82e5715914b1a
Geórgia Carin <georgia.carin@simplesmeiosdepagamento.com.br>
2024-11-14 09:57:51 -0300

    Ajuste de tema: adição das cores e classes

---
+++ src/components/tables/tb-receita.tsx
@@ -0,0 +1,23 @@
+import Table from '@mui/joy/Table';
+
+export default function TbReceita() {
+    
+    return (
+        <Table aria-label="basic table">
+        <thead>
+            <tr>
+            <th >Dessert (100g serving)</th>
+            <th>Calories</th>
+            <th>Fat&nbsp;(g)</th>
+            <th>Carbs&nbsp;(g)</th>
+            <th>Protein&nbsp;(g)</th>
+            </tr>
+        </thead>
+        <tbody>
+            {
+
+            }
+        </tbody>
+        </Table>
+    );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary-green': 'var(--primary-green)',
+        
+        'secondary-green': 'var(--secondary-green)',
+
+        'dark-blue': 'var(--dark-blue)',
+
+        'text-primary': 'var(--text-color-gray)',
+
+        'light-green-50': 'var(--light-green-50)',
+
+        'light-green-400': 'var(--light-green-400)',
+
+        red: 'var(--red)',
+
+        blue: 'var(--blue)',
+
+      }
+    },
   },
   plugins: [],
 }
